e58b3da8013fdb84a2d615d3fd01199ecc3b791a
nathan-nando <nathan.nandoo@gmail.com>
2023-08-16 10:09:28 +0700

    fix update newEntry

--- api/controller/newEntry.js
+++ api/controller/newEntry.js
@@ -45,7 +45,7 @@ export const updateOne = async (req, res) => {
 
     try {
         const result = await NewEntry.findByIdAndUpdate(id, updatedData);
-        res.status(200).json({data: result});
+        res.status(200).json({data: updatedData});
 
     } catch (e) {
         res.status(500).json({message: e.message});
